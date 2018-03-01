5359e3e787d0a402810104dd39e7fbcc493e7915
Siyavash Habashi <ghashange@live.com>
2018-03-01 21:21:33 +0100

    impl. getting jwt token on GET /token call.

--- spray-jwt-akka-http-test/src/main/scala/org/janjaali/sprayjwt/akkahttp/token/TokenRoutes.scala
+++ spray-jwt-akka-http-test/src/main/scala/org/janjaali/sprayjwt/akkahttp/token/TokenRoutes.scala
@@ -1,12 +1,19 @@
 package org.janjaali.sprayjwt.akkahttp.token
 
 import akka.http.scaladsl.server.{Directives, Route}
+import org.janjaali.sprayjwt.Jwt
+import org.janjaali.sprayjwt.algorithms.HS256
+import spray.json.{JsObject, JsString}
 
 class TokenRoutes extends Directives {
   val routes: Route = Route {
     path("token") {
-      get {
-        complete("dance")
+      rejectEmptyResponse {
+        get {
+          val payload = JsObject("dance" -> JsString("in the rain"))
+          val jwt = Jwt.encode(payload, "secret", HS256)
+          complete(jwt)
+        }
       }
     }
   }
